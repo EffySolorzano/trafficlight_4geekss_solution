73a76fbd797a356a7af016a0d18262321df52cf6
EffySolorzano <solorzano.steph@gmail.com>
2023-03-01 21:33:03 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,19 +7,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+				<p className="fixed-bottom">
+					Made by{" "}
+					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+					love!
+				</p>
+			</div>
 	);
 };
 
